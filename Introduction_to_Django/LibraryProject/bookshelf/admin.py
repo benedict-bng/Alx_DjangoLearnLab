6f84d0c50752f9efd804df931e17f974498b109a
Benedict Nderitu <giteebenedict@gmail.com>
2025-08-28 13:57:11 +0300

    Add files via upload

---
+++ Introduction_to_Django/LibraryProject/bookshelf/admin.py
@@ -0,0 +1,20 @@
+from django.contrib import admin
+from .models import Book
+
+# Custom admin class for Book model
+@admin.register(Book)  # This decorator registers the model
+class BookAdmin(admin.ModelAdmin):
+    # WHAT ADMIN SHOWS IN LIST VIEW
+    list_display = ('title', 'author', 'publication_year')
+    
+    # FILTERS ON THE RIGHT SIDEBAR
+    list_filter = ('author', 'publication_year')
+    
+    # SEARCH BOX AT THE TOP
+    search_fields = ('title', 'author')
+    
+    # PAGINATION - 25 BOOKS PER PAGE
+    list_per_page = 25
+    
+    # EDIT FIELDS DIRECTLY IN LIST VIEW
+    list_editable = ('publication_year',)
