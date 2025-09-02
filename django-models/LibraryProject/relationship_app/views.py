d9b2fc03605d22ead2e50b1c9cbf1fa30b026a0f
Benedict Nderitu <giteebenedict@gmail.com>
2025-09-03 02:43:08 +0300

    Update views.py

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -50,3 +50,34 @@ def logout_view(request):
     logout(request)
     return render(request, 'relationship_app/logout.html')
 
+from django.shortcuts import render
+from django.contrib.auth.decorators import user_passes_test, login_required
+
+# Helper functions to check user roles
+def is_admin(user):
+    return user.userprofile.role == 'Admin'
+
+def is_librarian(user):
+    return user.userprofile.role == 'Librarian'
+
+def is_member(user):
+    return user.userprofile.role == 'Member'
+
+# Admin view
+@login_required
+@user_passes_test(is_admin)
+def admin_view(request):
+    return render(request, 'relationship_app/admin_view.html')
+
+# Librarian view
+@login_required
+@user_passes_test(is_librarian)
+def librarian_view(request):
+    return render(request, 'relationship_app/librarian_view.html')
+
+# Member view
+@login_required
+@user_passes_test(is_member)
+def member_view(request):
+    return render(request, 'relationship_app/member_view.html')
+
