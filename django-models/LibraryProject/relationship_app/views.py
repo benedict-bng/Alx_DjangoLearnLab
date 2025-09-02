f36d2ca8ffc33ee2994b0aea535ec07600bd8b2f
Benedict Nderitu <giteebenedict@gmail.com>
2025-09-03 02:21:07 +0300

    Update views.py

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -12,4 +12,5 @@ class LibraryDetailView(DetailView):
     model = Library
     template_name = "relationship_app/library_detail.html"
     context_object_name = "library"
+["from .models import Library"]
 
