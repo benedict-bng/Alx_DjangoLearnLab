e78a3792f22dfe934eb39ef7b5dad8c88e86b316
Benedict Nderitu <giteebenedict@gmail.com>
2025-09-10 08:06:12 +0300

    Update models.py

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -45,3 +45,18 @@ class CustomUser(AbstractUser):
 
     def __str__(self):
         return self.username
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=200)
+    published_date = models.DateField()
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        permissions = [
+            ("can_view", "Can view book"),
+            ("can_create", "Can create book"),
+            ("can_edit", "Can edit book"),
+            ("can_delete", "Can delete book"),
+        ]
