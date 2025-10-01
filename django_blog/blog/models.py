d143d85b9a9404328a943831e977cfdca0c93df5
Benedict Nderitu <giteebenedict@gmail.com>
2025-10-02 00:42:12 +0300

    Update models.py

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -7,3 +7,15 @@ class Book(models.Model):
     
     def __str__(self):
         return f"{self.title} by {self.author} ({self.publication_year})"
+
+from django.db import models
+from django.contrib.auth.models import User
+
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    published_date = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="posts")
+
+    def __str__(self):
+        return self.title
