e84d83f3bcea10e1c3d3c664a00a2caf6055b427
Benedict Nderitu <giteebenedict@gmail.com>
2025-10-02 01:17:43 +0300

    Update urls.py

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -24,3 +24,22 @@ urlpatterns = [
     path('', include('blog.urls')),
 "post/<int:pk>/delete/", "post/<int:pk>/update/", "post/new/"
 ]
+from django.urls import path
+from .views import (
+    PostListView, PostDetailView,
+    PostCreateView, PostUpdateView, PostDeleteView,
+    add_comment, CommentUpdateView, CommentDeleteView
+)
+
+urlpatterns = [
+    path('posts/', PostListView.as_view(), name='post-list'),
+    path('posts/new/', PostCreateView.as_view(), name='post-create'),
+    path('posts/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
+    path('posts/<int:pk>/edit/', PostUpdateView.as_view(), name='post-edit'),
+    path('posts/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
+
+    # Comments
+    path('posts/<int:pk>/comments/new/', add_comment, name='add-comment'),
+    path('comments/<int:pk>/edit/', CommentUpdateView.as_view(), name='comment-edit'),
+    path('comments/<int:pk>/delete/', CommentDeleteView.as_view(), name='comment-delete'),
+]
